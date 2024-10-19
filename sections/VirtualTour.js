aea4f1bddffb70744052707b11d0364c86d791c9
darshpsd <hidarshpsd@gmail.com>
2024-10-19 18:17:25 +0530

    updated video link

--- sections/VirtualTour.js
+++ sections/VirtualTour.js
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 const VirtualTour = () => {
   return (
-    <section id="virtual-tour" className="hidden">
+    <section id="virtual-tour" className="">
       <div className="container">
         <div className="page-header">
           <h2 className="title">Virtual Tour</h2>
         </div>
         <div className="pricing--col">
           <div className="flex_">
-            {/* <video
-              src="/WhatsApp Video 2024-09-21 at 10.07.54_71263b67.mp4"
+            <video
+              src="https://res.cloudinary.com/dgzo0kdef/video/upload/v1729341861/next-foot-step/d5vigdckkbgspxt5rpba.mp4"
               type="mp4"
               controls="true"
               className="w-full"
-            ></video> */}
+            ></video>
           </div>
         </div>
       </div>
